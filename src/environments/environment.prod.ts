9757a43c41511630bd7b55f6579f6dceb7ff4bc2
unknown <xingkunyin.hotmail.com>
2019-07-07 20:35:43 -0400

    Finishing Setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA_V0TXZio9gPhJQKegyOO4iFl9t7ApxrU",
+    authDomain: "oshop-a700.firebaseapp.com",
+    databaseURL: "https://oshop-a700.firebaseio.com",
+    projectId: "oshop-a700",
+    storageBucket: "",
+    messagingSenderId: "466565629326",
+    appId: "1:466565629326:web:8e7f01da77859a31"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA_V0TXZio9gPhJQKegyOO4iFl9t7ApxrU",
+    authDomain: "oshop-a700.firebaseapp.com",
+    databaseURL: "https://oshop-a700.firebaseio.com",
+    projectId: "oshop-a700",
+    storageBucket: "",
+    messagingSenderId: "466565629326",
+    appId: "1:466565629326:web:8e7f01da77859a31"
+  }
 };
 
 /*
